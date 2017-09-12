f1038dd062197129e3adfe14aa0372cff28cc46d
Andre Joseph <ajcubeta@gmail.com>
2017-09-12 16:20:59 +0800

    User and Post models

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
   # root 'sessions#new'
-
   get 'logout' => 'sessions#destroy', :as => 'logout'
   get 'login'  => 'sessions#new',     :as => 'login'
   resources :sessions
+  resources :users
 end
