408d5ef9d60fca7edcabefc38fa884f5ea370e9d
Andre Joseph <ajcubeta@gmail.com>
2017-09-14 09:45:37 +0800

    Create posts index page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,10 @@
 class PostsController < ApplicationController
   http_basic_authenticate_with name: "jobline", password: "resources", except: :index
 
+  def index
+    @posts = Post.all
+  end
+
   def webhoook
     @title = "Webhook"
   end
