811fbbfe47502f770a2a0c7fed53953d7533294c
Andre Joseph <ajcubeta@gmail.com>
2017-09-21 18:15:53 +0800

    Set title on some pages

--- app/controllers/webhook_event_requests_controller.rb
+++ app/controllers/webhook_event_requests_controller.rb
@@ -12,57 +12,54 @@ class WebhookEventRequestsController < ApplicationController
       render json: @webhook_event_request, status: :created
     else
       render json: @webhook_event_request.errors,
-        status: :unprocessable_entity
+             status: :unprocessable_entity
     end
   end
 
-  # GET /webhook_event_requests
-  # GET /webhook_event_requests.json
   def index
     @webhook_event_requests = WebhookEventRequest.all
   end
 
-  # GET /webhook_event_requests/1
-  # GET /webhook_event_requests/1.json
   def show
   end
 
-  # GET /webhook_event_requests/new
   def new
     @webhook_event_request = WebhookEventRequest.new
   end
 
-  # GET /webhook_event_requests/1/edit
   def edit
   end
 
-  # POST /webhook_event_requests
-  # POST /webhook_event_requests.json
   def create
     @webhook_event_request = WebhookEventRequest.new(webhook_event_request_params)
 
     respond_to do |format|
       if @webhook_event_request.save
-        format.html { redirect_to @webhook_event_request,
+        format.html {
+          redirect_to @webhook_event_request,
           notice: 'Webhook event request was successfully created.'
         }
 
-        format.json { render action: 'show', status: :created,
-                             location: @webhook_event_request }
+        format.json {
+          render action: 'show',
+                 status: :created,
+                 location: @webhook_event_request
+        }
       else
         format.html { render action: 'new' }
-        format.json { render json: @webhook_event_request.errors,
-                             status: :unprocessable_entity }
+        format.json {
+          render json: @webhook_event_request.errors,
+                 status: :unprocessable_entity
+        }
       end
     end
   end
 
-  # PATCH/PUT /webhook_event_requests/1
-  # PATCH/PUT /webhook_event_requests/1.json
   def update
     respond_to do |format|
       if @webhook_event_request.update(webhook_event_request_params)
-        format.html { redirect_to @webhook_event_request,
+        format.html {
+          redirect_to @webhook_event_request,
           notice: 'Webhook event request was successfully updated.'
         }
         format.json { head :no_content }
